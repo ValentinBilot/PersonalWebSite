0b73b0f71bb1f8016b80de74055b858d09cbc01c
valentinbilot <valentin.bilot@gmail.com>
2019-10-14 15:42:37 +0200

    bootstrap js

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,24 @@ router.get('/cv', function(req, res, next) {
   res.render('CV');
 });
 
+router.get('/chordsgenerator', function(req, res, next) {
+  res.render('chordsgenerator');
+});
+
+router.get('/drumloopsgenerator', function(req, res, next) {
+  res.render('drumloopsgenerator');
+});
+
+router.get('/asc', function(req, res, next) {
+  res.render('asc');
+});
+
+router.get('/music', function(req, res, next) {
+  res.render('music');
+});
+
+
+
 router.get('/firstOSC', function(req, res, next) {
   res.render('firstOSC');
 });
